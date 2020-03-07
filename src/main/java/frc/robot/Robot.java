749e5e6e0f4e66cc1abc9a4e9ee2e661a0af1a13
Reiden <48872777+webr7818@users.noreply.github.com>
2020-03-07 00:55:15 -0800

    Spring Cleaning
    Cleaned variables and button mapping

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,8 +15,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-//import edu.wpi.first.wpilibj.SpeedController;
-//import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,32 +24,29 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * directory.
  */
 public class Robot extends TimedRobot {
-    /*SpeedController frontLeftBlow = new PWMVictorSPX(0);
-    SpeedController rearLeftBlow = new PWMVictorSPX(1);
-    SpeedControllerGroup left = new SpeedControllerGroup(frontLeftBlow, rearLeftBlow);
-    
-    SpeedController frontRightSuck = new PWMVictorSPX(2);
-    SpeedController rearRightSuck = new PWMVictorSPX(3);
-    SpeedControllerGroup right = new SpeedControllerGroup(frontRightSuck, rearRightSuck);
 
-    //SpeedController leftBlow = new PWMVictorSPX(5);
-    //SpeedController rightSuck = new PWMVictorSPX(6);
- 
-    DifferentialDrive robotDrive = new DifferentialDrive(left, right);*/
+  private Spark frontLeft;
+  private Spark rearLeft;
+  private Spark frontRight;
+  private Spark rearRight;
+  private Spark intake;
+  private Spark carry;
 
-    Spark frontLeft = new Spark(8);
-    Spark rearLeft = new Spark(9);
-    Spark frontRight = new Spark(0);
-    Spark rearRight = new Spark(1);
-    Spark intake = new Spark(7);
-    Spark carry = new Spark(2);
-   
-    SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft); 
-    SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
-    DifferentialDrive robotDrive = new DifferentialDrive(left, right);
-    
-    private final Joystick ps4 = new Joystick(0);
-    private final Timer timer = new Timer();
+  private SpeedControllerGroup left;
+  private SpeedControllerGroup right;
+  private DifferentialDrive robotDrive;
+
+  private static final int frontLeftPort = 8;
+  private static final int rearLeftPort = 9;
+  private static final int frontRightPort = 0;
+  private static final int rearRightPort = 1;
+  private static final int intakePort = 7;
+  private static final int carryPort = 2;
+
+  private Joystick ps4;
+  private static final int ps4Port = 0;
+
+  private final Timer timer = new Timer();
    
   /**
    * This function is run when the robot is first started up and should be
@@ -59,6 +54,22 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
+    // Assigning Spark ID
+    frontLeft = new Spark(frontLeftPort);
+    rearLeft = new Spark(rearLeftPort);
+    frontRight = new Spark(frontRightPort);
+    rearRight = new Spark(rearRightPort);
+    intake = new Spark(intakePort);
+    carry = new Spark(carryPort);
+
+    // Drivetrain
+    left = new SpeedControllerGroup(frontLeft, rearLeft); 
+    right = new SpeedControllerGroup(frontRight, rearRight);
+    robotDrive = new DifferentialDrive(left, right);
+    
+    // Assigning PS4 Controller ID
+    ps4 = new Joystick(ps4Port);
   }
 
   /**
@@ -76,7 +87,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     // Drive for 2 seconds
-    if (timer.get() < 5.0) {
+    if (timer.get() < 2.0) {
       robotDrive.arcadeDrive(0.5, 0.0); // drives forwards half speed
     } else {
       robotDrive.stopMotor(); // stop robot
@@ -98,41 +109,33 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
+    // Sensitivity
     double move_sensitivity = 0.4;
     double rotate_sensitivity = 0.5;
+
+    // Movement
     double ps4_move = ((ps4.getRawAxis(4) + 0.5) - (ps4.getRawAxis(3) + 0.5)) * move_sensitivity;
     double ps4_rotate = ps4.getX(Hand.kLeft) * rotate_sensitivity;
+    robotDrive.arcadeDrive(ps4_move, ps4_rotate);
 
-    if (ps4.getRawButton(1)==true)
-    {
-      rotate_sensitivity = 1.0;
-    }
-    if (ps4.getRawButton(1)==false)
-    {
-      rotate_sensitivity = 0.5;
+    // Button Mapping
+    if (ps4.getRawButton(2)) {
+      rotate_sensitivity = 0.8;
+    } else {
+      rotate_sensitivity = 0.4;
     } 
 
     if (ps4.getRawButton(5)) {
       intake.set(-0.5);
-    } else if (ps4.getRawButton(5) == false) { 
-      intake.stopMotor();
     }
 
     if (ps4.getRawButton(6)) {
       intake.set(0.5);
-    } else if (ps4.getRawButton(6) == false) { 
-      intake.stopMotor();
-    }
+    } 
 
     if (ps4.getRawAxis(5) != 0) {
       carry.set(ps4.getRawAxis(5) * -0.5);
-      //rightBlow.set(oshit);
-    } else {
-      carry.stopMotor();
     }
-    //rightBlow.set(oshit);
-    robotDrive.arcadeDrive(ps4_move, ps4_rotate);
-
   }
 
   /**
